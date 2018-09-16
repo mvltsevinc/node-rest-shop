00286862e9fa2bbdf7290eafe6606a22cca39ec7
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-09-16 23:46:58 +0300

    Handling Errors & Improving the Project Setup

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 
+// Handle incoming GET request to /orders
 router.get("/", (req, res, next) => {
     res.status(200).json({
         message: "Orders were fetched!"
--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express(); // Express uygulamasını app ye attık.Böylece app ile express deki tum fonksiyonları kullanabilirsin.
-
+const morgan = require("morgan"); // Logging package
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
 
-app.use("/products",productRoutes);
-app.use("/orders",orderRoutes);
+// Routes which should handle request
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found!");
+  error.status = 404;
+  next(error); // Default olan hata mesajını next fonksiyonu ile eziyoruz. Request yapıyoruz burada altta yakalıyoz
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
