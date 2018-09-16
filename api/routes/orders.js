7c855b0fff0c88fc6c76d95a22a8f825302e76b4
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-09-16 23:15:30 +0300

    Adding More Routes to the API

---
+++ api/routes/orders.js
@@ -0,0 +1,31 @@
+/*Product ile ilgili routerlerin handle edildigi dosya*/
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    res.status(200).json({
+        message: "Orders were fetched!"
+    });
+});
+
+router.post("/", (req, res, next) => {
+    res.status(201).json({ // 201 everything was successful resource created demek
+        message: "Orders was created!"
+    });
+});
+
+router.get("/:orderId", (req, res, next) => {
+    res.status(200).json({
+        message: "Order details!",
+        orderId : req.params.orderId
+    });
+});
+
+router.delete("/:orderId", (req, res, next) => {
+    res.status(200).json({
+        message: "Order deleted!",
+        orderId : req.params.orderId
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express(); // Express uygulamasını app ye attık.Böylece app ile express deki tum fonksiyonları kullanabilirsin.
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "Hello World!"
-  });
-});
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+
+app.use("/products",productRoutes);
+app.use("/orders",orderRoutes);
 
 module.exports = app;
